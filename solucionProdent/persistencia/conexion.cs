4f4591e768ba8c217377526724ac6d02a330d255
loopezcesar <cesar.1608@outlook.com>
2015-11-12 18:58:34 -0500

    estructura inicial

---
+++ solucionProdent/persistencia/conexion.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persistencia
+{
+    public class conexion
+    {
+        private SqlConnection conexion;
+        private SqlTransaction transaccion;
+
+        public void abrirConexion()
+        {
+            try
+            {
+                conexion = new SqlConnection();
+                conexion.ConnectionString = "Data Source=(local);Initial Catalog=prodent;Integrated Security=true";
+                conexion.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public void iniciarTransaccion()
+        {
+            try
+            {
+                abrirConexion();
+                transaccion = conexion.BeginTransaction();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void terminarTransaccion()
+        {
+            try
+            {
+                transaccion.Commit();
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void cancelarTransaccion()
+        {
+            try
+            {
+                transaccion.Rollback();
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public SqlDataReader ejecutarConsulta(String sentenciaSQL)
+        {
+            try
+            {
+                SqlCommand comando = conexion.CreateCommand();
+                if (transaccion != null)
+                    comando.Transaction = transaccion;
+                comando.CommandText = sentenciaSQL;
+                comando.CommandType = CommandType.Text;
+                SqlDataReader resultado = comando.ExecuteReader();
+                return resultado;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public SqlCommand obtenerComandoSQL(String sentenciaSQL)
+        {
+            try
+            {
+                SqlCommand comando = conexion.CreateCommand();
+                if (transaccion != null)
+                    comando.Transaction = transaccion;
+                comando.CommandText = sentenciaSQL;
+                comando.CommandType = CommandType.Text;
+                return comando;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public SqlCommand obtenerComandoSP(String procedimiento_almacenado)
+        {
+            try
+            {
+                SqlCommand comando = conexion.CreateCommand();
+                if (transaccion != null)
+                    comando.Transaction = transaccion;
+                comando.CommandText = procedimiento_almacenado;
+                comando.CommandType = CommandType.StoredProcedure;
+                return comando;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
---
+++ solucionProdent/presentacion/frm-login.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Iniciar sesion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "usuario";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(138, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "clave";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(138, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(162, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(121, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Recuperar clave";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
